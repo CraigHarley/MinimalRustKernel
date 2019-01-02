15235cb63f2acbe9cadec53cf7d9e6489856332f
Craig Harley <craigharley2@googlemail.com>
2019-01-02 23:45:52 +0000

    freestanding kernel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn mainCRTStartup() -> ! {
+    main();
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
